e88bc10efba5d0a559e01aaab007cfbda3c04725
John Saroka <john.saroka@actian.com>
2015-12-31 14:07:36 -0500

    vwsesslog

---
+++ vwsesslog.java
@@ -0,0 +1,78 @@
+//
+//  Session/Query Logger Process
+//
+//  2015-12-30 (sarjo01) Created.
+//
+import java.sql.*;
+
+class vwsesslog {
+
+   public static void main (String args[]) {
+
+      if (args.length < 2) {
+         System.out.println("Syntax: java vwsesslog <port> <interval_secs> [ new ]\n");
+         System.exit(0);
+      }
+      String droplog = "drop table if exists vwsesslog, vwqrylog";
+      String createlog = "create table vwsesslog as select " +
+         "s.*, int(0) as sesskey, int(0) as qkey, int(0) as session_et " +
+         "from ima_vwsesslog s where 1 = 0";
+      String createqlog = "create table vwqrylog as select " +
+         "session_id, sesskey, query_start_secs, qkey, session_lquery " +
+         "from vwsesslog where 1 = 0";
+      String createsnap = "declare global temporary table sesssnap as " +
+         "select s.* from vwsesslog s where 1 = 0 " +
+         "on commit preserve rows with norecovery";
+      String insertsnap = "insert into sesssnap select *, int(0), int(0), int(0) from ima_vwsesslog " +
+         "where db_owner != '$ingres' and db_owner != '' and " +
+         "effective_user not like '%>%' and effective_user != '$ingres'";
+      String upd1snap = "update sesssnap set session_lquery = trim(both ' ' from session_lquery)";
+      String upd2snap = "update sesssnap set " +
+         "sesskey=hash(varchar(session_id)||varchar(session_time)), " +
+         "qkey=hash(varchar(session_id)||varchar(query_start_secs)||varchar(session_lquery))";
+      String insertlog = "insert into vwsesslog select " +
+         "* from sesssnap " +
+         "where sesskey not in (select sesskey from vwsesslog)";
+      String insertqlog = "insert into vwqrylog select " +
+         "session_id, sesskey, query_start_secs, qkey, session_lquery " +
+         "from sesssnap where qkey not in " +
+         "(select qkey from vwqrylog)";
+      String updlogend = "update vwsesslog set session_et = unix_timestamp() " +
+         "where session_et = 0 and sesskey not in (select sesskey from sesssnap)";
+      String deletesnap = "delete from sesssnap";
+
+      String url = "jdbc:ingres://localhost:" + args[0] + "/imadb";
+      long interval = Long.parseLong(args[1]) * 1000; 
+      boolean newlog = false;
+      if (args.length == 3 && args[2].equals("new")) newlog = true;
+
+      Connection conn = null; 
+      Statement stmt = null;
+
+      try {
+         conn = DriverManager.getConnection(url);
+         conn.setAutoCommit(false);
+         stmt = conn.createStatement();
+         if (newlog) {
+            stmt.execute(droplog);
+            stmt.execute(createlog);
+            stmt.execute(createqlog);
+         }
+         stmt.execute(createsnap);
+         while (true) {
+            stmt.execute(insertsnap);
+            stmt.execute(upd1snap);
+            stmt.execute(upd2snap);
+            stmt.execute(insertlog);
+            stmt.execute(insertqlog);
+            stmt.execute(updlogend);
+            stmt.execute(deletesnap);
+            Thread.sleep(interval);
+         }
+      }
+      catch (SQLException sqlex) {
+         System.out.println(sqlex.getMessage() + "\n");
+      }
+      catch (InterruptedException ie) { }
+   }
+}
---
+++ vwsesslog.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+#
+# Session Logger
+#
+# 2015-12-08 (sarjo01) Created.
+# 2015-12-10 (sarjo01) Add session_id to list.
+# 2015-12-16 (sarjo01) Add queries.
+# 2015-12-30 (sarjo01) Add new ima table, client_terminal.
+#
+cmd=$1
+ii=`ingprenv II_INSTALLATION`
+dasport=${ii}7
+p=
+table=
+outfile=/tmp/vwsesslog${ii}.out
+iiup=$(echo "\q" | sql imadb | grep login)
+[ "$iiup" != "" ] && table=$(echo "help table vwsesslog\g" | sql -S imadb | grep Name:)
+syntax() {
+   echo
+   echo "Syntax: vwsesslog.sh start interval_seconds [ new ]"
+   echo "        vwsesslog.sh stop"
+   echo "        vwsesslog.sh status"
+   echo "        vwsesslog.sh list [ timestamp [ dbname ] ]"
+   echo "        vwsesslog.sh queries [ session_id ]"
+   echo
+   exit
+}
+imatable() {
+cat << IMATBL | sql -u\$ingres imadb 2>&1 >/dev/null
+drop table if exists ima_vwsesslog\\g
+register table ima_vwsesslog (
+   server varchar(64) not null not default is
+      'SERVER',
+   session_id varchar(32) not null not default is
+      'exp.scf.scs.scb_self',
+   effective_user varchar(32) not null not default is
+      'exp.scf.scs.scb_euser',
+   real_user varchar(32) not null not default is
+      'exp.scf.scs.scb_ruser',
+   db_name varchar(32) not null not default is
+      'exp.scf.scs.scb_database',
+   db_owner varchar(32) not null not default is
+      'exp.scf.scs.scb_dbowner',
+   client_host varchar(20) not null not default is
+      'exp.scf.scs.scb_client_host',
+   client_user varchar(32) not null not default is
+      'exp.scf.scs.scb_client_user',
+   client_terminal varchar(20) not null not default is
+      'exp.scf.scs.scb_client_tty',
+   session_time integer not null not default is
+      'exp.clf.unix.cs.scb_connect',
+   query_start_secs integer4 not null not default is
+      'exp.scf.scs.scb_query_start_secs',
+   session_lquery varchar(250) not null not default is
+      'exp.scf.scs.scb_lastquery' )
+as import from 'tables'
+with dbms = IMA, structure = unique sortkeyed,
+key = (server, session_id)\\g
+grant all on ima_vwsesslog to public with grant option\\g
+IMATBL
+}
+setp() {
+   p=$(ps -fade | grep vwsesslog.jar | grep $dasport | grep -v grep | awk '{print $2}')
+}
+status() {
+   setp
+   if [[ -n $p ]]
+   then
+      echo Started...
+   else
+      echo Stopped. && [ -s $outfile ] && echo Last error: && cat $outfile
+   fi
+}
+stoplog() {
+   setp
+   [ "$p" != "" ] && kill $p 
+}
+chkconnect() {
+   [ "$iiup" == "" ] && echo && echo "Can't connect to imadb. Installation down?" && echo && exit
+}
+
+[ $# -lt 1 ] && syntax
+if [ "$cmd" == "queries" ]
+then
+   [ "$2" != "" ] && session_id="where varchar(session_id, 22) = '$2'"
+   chkconnect
+   echo
+   echo "Session_ID             Query_started       Query_text"
+   echo "---------------------- ------------------- ------------------------------------------------------------"
+   cat << QUERIES | sql -S imadb
+\silent
+\notitles
+select varchar(session_id, 22),
+varchar(timestamp(from_unixtime(query_start_secs)), 19),
+varchar(session_lquery, 60)
+from vwqrylog $session_id order by 2\\g
+QUERIES
+echo
+status
+echo
+else
+if [ "$cmd" == "status" ]
+then
+   status
+else
+if [ "$cmd" == "stop" ]
+then
+   stoplog 
+   status
+else
+if [ "$cmd" == "start" ]
+then
+   [[ $2 != *[[:digit:]]* ]] && syntax
+   interval=$2
+   chkconnect
+   stoplog 
+   imatable
+   if [[ -z $table ]]
+   then
+      new=new
+   else
+      new=$3
+   fi
+   setsid java -cp ./vwsesslog.jar:$II_SYSTEM/ingres/lib/iijdbc.jar vwsesslog $dasport $interval $new >$outfile 2>&1 < /dev/null &
+   sleep 2
+   status 
+else
+[ "$cmd" != "list" ] && syntax
+chkconnect
+[ "$table" == "" ] && echo && echo "Logging table not initialized." && echo && exit
+ts=
+tdb=
+[ "$2" != "" ] && ts="where '$2' between start_time and end_time"
+[ "$3" != "" ] && tdb="and db_name = '$3'"
+echo
+echo "Session_ID             Client_host  Client_term  Client_user  Eff_user     Real_user    DB_name      Session_started     Session_ended_by"
+echo "---------------------- ------------ ------------ ------------ ------------ ------------ ------------ ------------------- -------------------"
+cat << SELECT | sql -S imadb
+\silent
+\notitles
+with t as (select
+varchar(session_id, 22) as session_id,
+varchar(client_host, 12) as client_host,
+varchar(client_terminal, 12) as client_terminal,
+varchar(client_user, 12) as client_user,
+varchar(effective_user, 12) as effectiver_user,
+varchar(real_user, 12) as real_user,
+varchar(db_name, 12) as db_name,
+varchar(timestamp(from_unixtime(session_time)), 19) as start_time,
+case when session_et = 0 then '                   ' else
+varchar(timestamp(from_unixtime(session_et)), 19) end as end_time
+from vwsesslog)
+select * from t $ts $tdb order by start_time\\g
+SELECT
+echo
+status
+echo
+fi
+fi
+fi
+fi
