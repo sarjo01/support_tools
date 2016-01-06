1898867ed83e1dfb7ed08e7bf26b69594f265e9a
John Saroka <john.saroka@actian.com>
2016-01-06 14:13:16 -0500

    Bug fix in case stmt

--- vwsesslog.sh
+++ vwsesslog.sh
@@ -8,7 +8,6 @@
 # 2015-12-30 (sarjo01) Add new ima table, client_terminal.
 # 2016-01-05 (sarjo01) Use case stmt for commands.
 #
-cmdlist="start stop status list queries"
 ii=`ingprenv II_INSTALLATION`
 dasport=${ii}7
 p=
